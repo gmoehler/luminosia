a873eecaf91afc856ea36d7eade1797aa0fe7a07
* <gmoeh@gmx.de>
2018-11-16 10:24:18 +0100

    prepare restruct with controls

---
+++ src/components/AudioControlProvider.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { loadAudio, playAudio, stopAudio } from '../actions/audioActions'
+import AudioControl from './AudioControl';
+
+const scale = window.devicePixelRatio;
+
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+const audioContext = new window.AudioContext();
+
+class AudioControlProvider extends Component {
+
+  doLoadAudio = (event) => {
+    this.props.loadAudioAction({
+      audioSource: "media/audio/Vocals30.mp3",
+      audioContext
+    });
+  }
+
+  render() {
+
+    return (
+      <AudioControl
+        loadAudio={ this.doLoadAudio } 
+        playAudio={ this.props.playAudioAction }
+        stopAudio={ this.props.stopAudioAction } />
+      );
+  }
+}
+
+const mapStateToProps = state => ({
+  // no props for now
+});
+
+const mapDispatchToProps = dispatch => ({
+  loadAudioAction: spec => dispatch(loadAudio(spec)),
+  playAudioAction: () => dispatch(playAudio()),
+  stopAudioAction: () => dispatch(stopAudio())
+})
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(AudioControlProvider);
