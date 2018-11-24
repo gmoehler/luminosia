6f59464543bdbe98c80e67f6e085ec288eea9302
Gregor Moehler <gmoeh@gmx.de>
2018-11-24 08:53:36 +0100

    remove playreducer

--- src/components/AudioGroupContainer.js
+++ src/components/AudioGroupContainer.js
@@ -4,7 +4,6 @@ import { connect } from 'react-redux';
 import AudioGroup from './AudioGroup'
 import { select, setChannelPlayState } from '../actions/audioActions'
 import { getAllChannelData } from '../reducers/audioReducer'
-import { getPlayState, getChannelPlayStates } from '../reducers/playReducer'
 import { getSelectionRange } from '../reducers/selectionReducer'
 
 class AudioGroupContainer extends Component {
@@ -22,7 +21,6 @@ const mapStateToProps = (state, props) => {
   // get audio data and play state from redux
   return {
     allAudioData: getAllChannelData(state),
-    playState: getPlayState(state),
     selection: getSelectionRange(state),
   }
 };
