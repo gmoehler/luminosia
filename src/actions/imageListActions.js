c39321d74803260a85fbd4062223238c43f6a868
Gregor Möhler <gmoehler@users.noreply.github.com>
2019-01-16 23:51:02 +0100

    remove all legacy load actions (#14)

--- src/actions/imageListActions.js
+++ src/actions/imageListActions.js
@@ -1,6 +1,6 @@
 import LoaderFactory from '../loader/LoaderFactory'
 
-import { ADD_IMAGE, CLEAR_IMAGELIST, REMOVE_IMAGE, LOAD_IMAGELIST_STARTED, LOAD_IMAGELIST_SUCCESS, LOAD_IMAGELIST_FAILURE } from './types';
+import { ADD_IMAGE, CLEAR_IMAGELIST, REMOVE_IMAGE } from './types';
 import { samplesToSeconds } from '../utils/conversions';
 
 
@@ -18,20 +18,6 @@ export const removeImage = (imageInfo) => ({
   payload: imageInfo
 });
 
-const loadImageListStarted = () => ({
-  type: LOAD_IMAGELIST_STARTED
-});
-
-const loadImageListSuccess = imageListInfo => ({
-  type: LOAD_IMAGELIST_SUCCESS,
-  payload: imageListInfo
-});
-
-const loadImageListFailure = errorInfo => ({
-  type: LOAD_IMAGELIST_FAILURE,
-  payload: errorInfo
-});
-
 function loadImageFromFile(imageSrc) {
   const loader = LoaderFactory.createLoader(imageSrc);
   return loader.load();
@@ -45,35 +31,5 @@ export function loadImage(imageInfo) {
     return img
   });
 }
-
-export const loadImageList = (({images, sampleRate}) => (dispatch) => {
-  dispatch(loadImageListStarted());
-
-  const loadImagesPromises = images
-    .map((fileConfig) => loadImageFromFile(fileConfig.src));
-
-
-  return Promise.all(loadImagesPromises)
-    .then((imageBuffers) => {
-
-      const normalizedImages = {};
-      imageBuffers.forEach((img) => {
-        img.sampleRate = sampleRate;
-        img.duration = samplesToSeconds(img.width, sampleRate)
-        normalizedImages[img.src] = img;
-
-      })
-
-      dispatch(loadImageListSuccess({
-        normalizedImages
-      }));
-    })
-
-    .catch(err => {
-      dispatch(loadImageListFailure({
-        err
-      }));
-    });
-});
 
 
