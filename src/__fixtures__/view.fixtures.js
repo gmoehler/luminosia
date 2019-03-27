9e19b85b0c552d9c28519664344750d7ddb284cd
* <gmoeh@gmx.de>
2019-03-27 22:58:27 +0100

    more tests

---
+++ src/__fixtures__/view.fixtures.js
@@ -0,0 +1,8 @@
+export const marker1={
+	        markerId: "anId",
+            channelId: 21,
+            partId: "aPartId",
+            pos: 21.5,
+            minPos: 2.6,
+            type: "aType",
+}
--- src/reducers/__tests__/viewReducer.test.js
+++ src/reducers/__tests__/viewReducer.test.js
@@ -2,6 +2,7 @@ import reducer from "../viewReducer";
 import { initialState } from "../viewReducer";
 import * as types from "../../actions/types";
 import { part, part2 } from "../../__fixtures__/channel.fixtures";
+import { marker1 } from "../../__fixtures__/view.fixtures";
 
 describe("view reducer", () => {
   it("should return the initial state", () => 
@@ -76,7 +77,54 @@ describe("view reducer", () => {
     expect(reducer3).toEqual(expectedState3);
   });
 
+it("should handle CLEAR_SEL for parts", () => {
 
+    const expectedState2 = {
+      ...initialState,
+      byMarkerId: {
+        [part.partId]: part,
+        [part2.partId]: part2
+      }
+    };
 
+    const expectedState3 = {
+      ...initialState,
+      selectedElementsById: { }
+    };
+
+    const reducer1 = reducer(reducer(undefined, {}), {
+      type: types.ADD_ELEMENT_TO_SEL,
+      payload: part
+    });
+
+    const reducer2 = reducer(reducer1,  {
+      type: types.ADD_ELEMENT_TO_SEL,
+      payload: part2
+    });
+
+    const reducer3 = reducer(reducer1,  {
+      type: types.CLEAR_SEL
+    });
+
+    expect(reducer2).toEqual(expectedState2);
+    expect(reducer3).toEqual(expectedState3);
+  });
+  
+  it("should handle SET_MARKER", () => {
+
+    const expectedState1 = {
+      ...initialState,
+      byMarkerId: {
+        [marker1.markerId]: marker1,
+      }
+    };
+
+    const reducer1 = reducer(reducer(undefined, {}), {
+      type: types.SET_MARKER,
+      payload: marker1
+    });
+
+    expect(reducer1).toEqual(expectedState1);
+  });
 
 });
