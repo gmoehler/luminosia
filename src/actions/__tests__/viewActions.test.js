d45265cf4f7b59d2bd600641d14fd2d78f213d85
Gregor Moehler <gmoeh@gmx.de>
2019-06-21 23:46:11 +0200

    test: remove app.test.js & add view action tests

--- src/actions/__tests__/viewActions.test.js
+++ src/actions/__tests__/viewActions.test.js
@@ -110,3 +110,278 @@ it("should paste a part from channel 2 to channel 1", () => {
   expect(acts).toEqual(expectedActions);
 
 });
+
+it("should add a part to multiselection", () => {
+
+  const state = {
+    ...imageChannelState,
+    view: {
+      ...initialViewState,
+      partsToCopy: [
+        part
+      ],
+      selectedImageChannelId: 1,
+    }
+  };
+
+  const store = mockStore(state);
+
+  const expectedActions = [{
+    type: types.ADD_ELEMENT_TO_SEL,
+    payload: part
+  }, {
+    type: types.UPDATE_MARKER,
+    payload: {
+      incr: undefined,
+      markerId: "2:1-l",
+      pos: undefined,
+      type: "selected"
+    }
+  }, {
+    type: types.UPDATE_MARKER,
+    payload: {
+      incr: undefined,
+      markerId: "2:1-r",
+      pos: undefined,
+      type: "selected"
+    }
+  }
+  ];
+
+  store.dispatch(actions.addPartToMultiSelection(part));
+  const acts = store.getActions();
+  expect(acts).toEqual(expectedActions);
+
+});
+
+
+it("should deselect a part in multiselection", () => {
+
+  const state = {
+    ...imageChannelState,
+    view: {
+      ...initialViewState,
+      partsToCopy: [
+        part
+      ],
+      selectedElementsById: {
+        [part.partId]: part
+      },
+      selectedImageChannelId: 1,
+    }
+  };
+
+  const store = mockStore(state);
+
+  const expectedActions = [{
+    type: types.SELECT_IMAGE_CHANNEL,
+    payload: part
+  }, {
+    type: types.REMOVE_ELEMENT_FROM_SEL,
+    payload: part,
+  }, {
+    type: types.UPDATE_MARKER,
+    payload: {
+      incr: undefined,
+      markerId: "2:1-l",
+      pos: undefined,
+      type: "normal"
+    }
+  }, {
+    type: types.UPDATE_MARKER,
+    payload: {
+      incr: undefined,
+      markerId: "2:1-r",
+      pos: undefined,
+      type: "normal"
+    }
+  }
+  ];
+
+  store.dispatch(actions.toggleElementMultiSelection(part));
+  const acts = store.getActions();
+  expect(acts).toEqual(expectedActions);
+
+});
+
+it("should select a part in multiselection", () => {
+
+  const state = {
+    ...imageChannelState,
+    view: {
+      ...initialViewState,
+      partsToCopy: [
+        part
+      ],
+      selectedElementsById: {
+      },
+      selectedImageChannelId: 1,
+    }
+  };
+
+  const store = mockStore(state);
+
+  const expectedActions = [{
+    type: types.SELECT_IMAGE_CHANNEL,
+    payload: part
+  }, {
+    type: types.CLEAR_SEL,
+  }, {
+    type: types.ADD_ELEMENT_TO_SEL,
+    payload: part,
+  }, {
+    type: types.UPDATE_MARKER,
+    payload: {
+      incr: undefined,
+      markerId: "2:1-l",
+      pos: undefined,
+      type: "selected"
+    }
+  }, {
+    type: types.UPDATE_MARKER,
+    payload: {
+      incr: undefined,
+      markerId: "2:1-r",
+      pos: undefined,
+      type: "selected"
+    }
+  }
+  ];
+
+  store.dispatch(actions.toggleElementMultiSelection(part));
+  const acts = store.getActions();
+  expect(acts).toEqual(expectedActions);
+
+});
+
+it("should sync markers for a part", () => {
+
+  const state = {
+    ...imageChannelState,
+    view: {
+      ...initialViewState,
+      partsToCopy: [
+        part
+      ],
+      selectedElementsById: {
+      },
+      selectedImageChannelId: 1,
+    }
+  };
+
+  const store = mockStore(state);
+
+  const expectedActions = [{
+    type: types.UPDATE_MARKER,
+    payload: {
+      markerId: "2:1-l",
+      pos: 3.3,
+    }
+  }, {
+    type: types.UPDATE_MARKER,
+    payload: {
+      markerId: "2:1-r",
+      pos: 3.3 + 11.21,
+
+    }
+  }
+  ];
+
+  store.dispatch(actions.syncMarkersForPart(part.channelId, part.partId));
+  const acts = store.getActions();
+  expect(acts).toEqual(expectedActions);
+
+});
+
+
+
+it("should update selected markers for part", () => {
+
+  const state = {
+    ...imageChannelState,
+    view: {
+      ...initialViewState,
+      partsToCopy: [
+        part
+      ],
+      selectedElementsById: {
+        [part.partId]: part
+      },
+      selectedImageChannelId: 1,
+    }
+  };
+
+  const store = mockStore(state);
+
+  const expectedActions = [{
+    type: types.UPDATE_MARKER,
+    payload: {
+      markerId: "2:1-l",
+      incr: 23,
+      pos: undefined,
+      type: "selected"
+    }
+  }, {
+    type: types.UPDATE_MARKER,
+    payload: {
+      markerId: "2:1-r",
+      incr: 23,
+      pos: undefined,
+      type: "selected"
+    }
+  }
+  ];
+
+  store.dispatch(actions.updateSelectedMarkers({
+    ...part,
+    incr: 23
+  }));
+  const acts = store.getActions();
+  expect(acts).toEqual(expectedActions);
+
+});
+
+it("should sync markers for a part", () => {
+
+  const state = {
+    ...imageChannelState,
+    view: {
+      ...initialViewState,
+      partsToCopy: [
+        part
+      ],
+      selectedElementsById: {
+        [part.partId]: part
+      },
+      selectedImageChannelId: 1,
+    }
+  };
+
+  const store = mockStore(state);
+
+  const expectedActions = [{
+    type: types.UPDATE_MARKER,
+    payload: {
+      markerId: "2:1-l",
+      incr: undefined,
+      pos: undefined,
+      type: "normal"
+    }
+  }, {
+    type: types.UPDATE_MARKER,
+    payload: {
+      markerId: "2:1-r",
+      incr: undefined,
+      pos: undefined,
+      type: "normal"
+    }
+  }, {
+    type: types.CLEAR_SEL
+  }
+  ];
+
+  store.dispatch(actions.clearElementSelectionWithMarkers());
+  const acts = store.getActions();
+  expect(acts).toEqual(expectedActions);
+
+});
